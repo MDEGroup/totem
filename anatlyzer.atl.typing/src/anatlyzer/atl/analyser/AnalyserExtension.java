b1ec5922d109aa47f58b9fb411416e33d4d08370
totem-mde <juri.di.rocco@gmail.com>
2017-11-23 15:58:33 +0100

    Removed old anatlyzer libraries

--- anatlyzer.atl.typing/src/anatlyzer/atl/analyser/AnalyserExtension.java
+++
@@ -1,9 +0,0 @@
-package anatlyzer.atl.analyser;
-
-import anatlyzer.atl.analyser.namespaces.GlobalNamespace;
-import anatlyzer.atl.model.ATLModel;
-import anatlyzer.atlext.ATL.Unit;
-
-public interface AnalyserExtension {
-	public void perform(ATLModel model, GlobalNamespace mm, Unit root);
-}
