53cf8b7b1c1ca2854f39e2633c77399c0ca9aec3
totem-mde <juri.di.rocco@gmail.com>
2017-12-22 18:00:33 +0100

    Added java services to set class as concrete

---
+++ totem.drm.editor/src/totem/drm/editor/SetToConcrete.java
@@ -0,0 +1,31 @@
+package totem.drm.editor;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+
+import MM_uncertainty.UBoolean;
+import MM_uncertainty.UnknowClass;
+
+public class SetToConcrete implements IExternalJavaAction {
+
+	public SetToConcrete() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute(Collection<? extends EObject> selections, Map<String, Object> parameters) {
+		for (EObject eObject : selections) {
+			MM_uncertainty.Class c=(MM_uncertainty.Class)eObject;
+			c.setIsAbstract(UBoolean.FALSE);
+		}
+	}
+
+	@Override
+	public boolean canExecute(Collection<? extends EObject> selections) {
+		return true;
+	}
+
+}
